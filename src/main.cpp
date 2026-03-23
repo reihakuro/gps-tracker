a9f7ec02af26c245a2f64c590d8d4734da97d43e
zRei <kientrungpham1910@gmail.com>
2026-03-23 08:37:33 +0700

    Build Frame

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,48 @@
 #include <Arduino.h>
-
 // put function declarations here:
-int myFunction(int, int);
+void wifiSetup(void *parameter){
+  // WiFi setup code here
+  for (;;){
+    // WiFi management code here
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS); 
+  }
+}
+
+void readGPS(void *parameter) {
+  for (;;){
+    // read GPS data
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS); 
+  }
+}
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+  xTaskCreatePinnedToCore(
+    readGPS,          // Function that implements the task.
+    "Read GPS",  // Text name for the task.
+    10000,            // Stack size in words, not bytes.
+    NULL,             // Parameter passed into the task.
+    1,                // Priority at which the task is created.
+    NULL,             // Used to pass out the created task's handle.
+    1                // Core where the task should run. 0 or 1.
+  );
 
+  xTaskCreatePinnedToCore(
+    wifiSetup,        // Function that implements the task.
+    "WiFi Setup",  // Text name for the task.
+    10000,            // Stack size in words, not bytes.
+    NULL,             // Parameter passed into the task.
+    1,                // Priority at which the task is created.
+    NULL,             // Used to pass out the created task's handle.
+    0               // Core where the task should run. 0 or 1.
+  );
+
+}
 void loop() {
   // put your main code here, to run repeatedly:
+  vTaskDelete(NULL);
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
